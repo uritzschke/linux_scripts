93ca85880d8749c32cb5ab5ac57163dfeaaa82b6
uritzschke <saxophant@gmail.com>
2017-10-06 13:11:32 +0200

    Create check_owncloud.sh

---
+++ check_owncloud.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Small script to check if Owncloud is currently running or in maintanence mode.
+# To be used with Monit (https://mmonit.com/)
+#
+# Requires jq JSON processor
+#
+#
+# Monit-Entry:
+#
+# check program owncloud with path /etc/monit/check_owncloud.sh
+#  if status != 0 then alert
+#
+#
+
+
+STATUS=`curl -s 'SERVER-URL/owncloud/status.php' | jq -r '.maintenance'`
+
+if [ "$STATUS" == "false" ]
+then
+  echo "Owcloud running"
+  exit 0
+elif [ "$STATUS" == "true" ]
+then
+  echo "Owcloud in MAINTENANCE MODE"
+  exit 1
+else
+  echo "Owncloud NOT RUNNING"
+  exit 2
+fi
